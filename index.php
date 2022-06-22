9c3820762cbbccacfc3e3365568ab4115656da04
tahakyo <ghazouani1988@email.com>
2022-06-22 15:10:33 +0200

    data_base connected

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    define('DB_SERVERNAME', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'root');
+    define('DB_NAME', 'db_university');
+    define('DB_PORT', 8889);
+    
+    $conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+
+    if ($conn && $conn->connect_error) {
+      echo "DB connection error" , $conn->connect_error;
+      die();
+    }
+    
+?>
